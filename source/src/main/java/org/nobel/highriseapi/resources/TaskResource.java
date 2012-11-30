85a487852fd2d0fc8f84f19e9fdff8ef6fed2ac4
Dennis Nobel <dennis@dnobel.de>
2012-11-30 19:31:07 +0100

    Fixed TaskResource and maven build.

--- source/src/main/java/org/nobel/highriseapi/resources/TaskResource.java
+++ source/src/main/java/org/nobel/highriseapi/resources/TaskResource.java
@@ -4,59 +4,64 @@ import java.util.List;
 
 import org.nobel.highriseapi.entities.Task;
 import org.nobel.highriseapi.resources.base.EntityCacheProvider;
+import org.nobel.highriseapi.resources.base.EntityCacheProvider.EntityCache;
 import org.nobel.highriseapi.resources.base.EntityResource;
 import org.nobel.highriseapi.resources.base.RestResourceConfig;
-import org.nobel.highriseapi.resources.base.EntityCacheProvider.EntityCache;
-
 
 public class TaskResource extends EntityResource<Task> {
 
-    public enum TaskList {
-        COMPLETED(1, "tasks/completed.xml"), UPCOMING(0, "tasks/upcoming.xml");
-        public static TaskList getById(int id) {
-            for (TaskList taskList : TaskList.values()) {
-                if (taskList.id == id) {
-                    return taskList;
-                }
-            }
-            return null;
-        }
+	public enum TaskList {
+		COMPLETED(1, "tasks/completed.xml"), UPCOMING(0, "tasks/upcoming.xml");
+		public static TaskList getById(int id) {
+			for (TaskList taskList : TaskList.values()) {
+				if (taskList.id == id) {
+					return taskList;
+				}
+			}
+			return null;
+		}
 
-        public int id;
+		public int id;
 
-        public String resourceUrl;
+		public String resourceUrl;
 
-        TaskList(int id, String resourceUrl) {
-            this.id = id;
-            this.resourceUrl = resourceUrl;
-        }
-    }
+		TaskList(int id, String resourceUrl) {
+			this.id = id;
+			this.resourceUrl = resourceUrl;
+		}
+	}
 
-    public TaskResource(String baseUrl, String token, EntityCacheProvider entityCacheProvider) {
-        super(baseUrl, token, entityCacheProvider);
-    }
+	@Override
+	public void clear() {
+		super.clear();
+		getCache(TaskList.COMPLETED.name()).clear();
+	}
 
-    public List<Task> getCompleted() {
+	public TaskResource(String baseUrl, String token, EntityCacheProvider entityCacheProvider) {
+		super(baseUrl, token, entityCacheProvider);
+	}
 
-        Class<?> type = getEntityListConfig().type;
-        String url = buildResourceUrl(getBaseUrl(), TaskList.COMPLETED.resourceUrl);
-        EntityCache cache = getCache(TaskList.COMPLETED.name());
+	public List<Task> getCompleted() {
 
-        return new RemoteEntityAccessorWithCacheSupport<Task>(url, type, cache).getEntityList();
-    }
+		Class<?> type = getEntityListConfig().type;
+		String url = buildResourceUrl(getBaseUrl(), TaskList.COMPLETED.resourceUrl);
+		EntityCache cache = getCache(TaskList.COMPLETED.name());
 
-    public List<Task> getUpcoming() {
-        return getAll();
-    }
+		return new RemoteEntityAccessorWithCacheSupport<Task>(url, type, cache).getEntityList();
+	}
 
-    @Override
-    protected RestResourceConfig getEntityConfig() {
-        return createResourceConfig("taks/{id}.xml", Task.class);
-    }
+	public List<Task> getUpcoming() {
+		return getAll();
+	}
 
-    @Override
-    protected RestResourceConfig getEntityListConfig() {
-        return createResourceConfig(TaskList.UPCOMING.resourceUrl, TaskList.class);
-    }
+	@Override
+	protected RestResourceConfig getEntityConfig() {
+		return createResourceConfig("tasks/{id}.xml", Task.class);
+	}
+
+	@Override
+	protected RestResourceConfig getEntityListConfig() {
+		return createResourceConfig(TaskList.UPCOMING.resourceUrl, org.nobel.highriseapi.entities.lists.TaskList.class);
+	}
 
 }
