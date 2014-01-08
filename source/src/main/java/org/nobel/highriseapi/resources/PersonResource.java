ed3f351a44409528f52ae509c291eeb5496be02b
Heikki Pora <heikki.pora@reaktor.fi>
2014-01-09 00:17:02 +0200

    Introduce search() and searchByEmail() to PersonResource

--- source/src/main/java/org/nobel/highriseapi/resources/PersonResource.java
+++ source/src/main/java/org/nobel/highriseapi/resources/PersonResource.java
@@ -7,8 +7,14 @@ import org.nobel.highriseapi.resources.base.EntityCacheProvider;
 import org.nobel.highriseapi.resources.base.EntityResource;
 import org.nobel.highriseapi.resources.base.RestResourceConfig;
 
-public class PersonResource extends EntityResource<Person> {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import static org.springframework.util.StringUtils.collectionToDelimitedString;
+
+public class PersonResource extends EntityResource<Person> {
     public PersonResource(HighriseClientConfig clientConfig, EntityCacheProvider entityCacheProvider) {
         super(clientConfig, entityCacheProvider);
     }
@@ -23,4 +29,25 @@ public class PersonResource extends EntityResource<Person> {
         return createResourceConfig("people.xml", PersonList.class);
     }
 
+    @SuppressWarnings("unchecked")
+    public List<Person> searchByEmail(final String address) {
+        Map<String, String> criteria = new HashMap<String, String>() {{
+            put("email", address);
+        }};
+        return search(criteria);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Person> search(Map<String, String> criteria) {
+        String url = withCriteria(buildResourceUrl(getBaseUrl(), "people/search.xml"), criteria);
+        return createRemoteEntityAccesor(PersonList.class, url).getEntityList();
+    }
+
+    protected String withCriteria(String url, Map<String, String> parameters) {
+        List<String> criteria = new ArrayList<String>();
+        for (String parameter : parameters.keySet()) {
+            criteria.add("criteria[" + parameter + "]=" + parameters.get(parameter));
+        }
+        return url + "?" + collectionToDelimitedString(criteria, "&");
+    }
 }
