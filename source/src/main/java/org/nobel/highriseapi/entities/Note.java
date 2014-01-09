ff58c5819a946b57bbdf2fc1eabf2c955e4cf230
Heikki Pora <heikki.pora@reaktor.fi>
2014-01-09 18:29:34 +0200

    Add attachments list to Note

--- source/src/main/java/org/nobel/highriseapi/entities/Note.java
+++ source/src/main/java/org/nobel/highriseapi/entities/Note.java
@@ -1,7 +1,12 @@
 package org.nobel.highriseapi.entities;
 
+import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
+
+import java.util.List;
 
 @Root(name = "note", strict = false)
 public class Note extends Recording {
+    @ElementList(name = "attachments", required = false)
+    private List<Attachment> attachments;
 }
